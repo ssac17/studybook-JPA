92349a160d4ed0dc9cb17713421331fe70a6bb7c
ssac17 <ghkdgksmf13@naver.com>
2024-04-24 10:57:05 +0900

    추가 : MessageRepository.java 인터페이스

---
+++ src/main/java/com/manning/javapersistence/ch02/repositories/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.manning.javapersistence.ch02.repositories;
+
+import com.manning.javapersistence.ch02.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+}
